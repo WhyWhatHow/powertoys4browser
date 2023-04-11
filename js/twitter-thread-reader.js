d2f59cb35cfaf7c7f9805b06af8c5f45d28d4184
whywhathow <whywhathow.fun@gmail.com>
2023-04-11 15:12:30 +0800

    fix: fix a bug that I can't use this script before refresh web page.

--- js/twitter-thread-reader.js
+++ js/twitter-thread-reader.js
@@ -1,29 +1,19 @@
 // ==UserScript==
 // @name         Twitter thread reader
 // @namespace    https://whywhathow.github.io/
-// @version      1.2
+// @version      1.3
 // @description  when we read twitter thread, it's sucks. so I try to generate twitter thread reader in one single page, so i can easily read and share. Anyway, it's easy to read twitter's thread.
 // @author       whywhathow
 // @match        https://twitter.com/*/status/*
 // @icon         https://abs.twimg.com/favicons/twitter.2.ico
 // @grant        unsafeWindow
 // @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
 // @grant        GM_registerMenuCommand
-// @run-at       document-idle
 // @updateURL    https://raw.githubusercontent.com/WhyWhatHow/powertoys4browser/master/js/twitter-thread-reader.js
 // @license      MIT
 // ==/UserScript==
 
-let curUrl = window.location.href;
-console.log(curUrl);
-
-// function handleUrl(curUrl) {
-//     if (curUrl.includes("/status/")) {
-//         return curUrl;
-//     } else {
-//         return null;
-//     }
-// }
 function getThreadID(url) {
     const arr = url.split("/");
     var threadId = arr[arr.length - 1];
@@ -39,13 +29,13 @@ function redirect(url) {
     // 跳转到指定 URL
     // window.location.href=url;
     GM_openInTab(url);
-    
+
 }
-// actually doing something 
+
+// actually doing something
 function running() {
     // 1. get url and twitter's thread id.
-
-    let currentUrl = curUrl;
+    let currentUrl = window.location.href;
     // if (currentUrl === null) {
     //     return;
     // }
@@ -70,9 +60,11 @@ function running() {
             console.error(error);
         }
     });
+
 }
+
 (function () {
     'use strict';
-    GM_registerMenuCommand('read thread in another page', running);   
+    GM_registerMenuCommand('read thread in another page', running);
 
 })();
