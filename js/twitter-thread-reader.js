e4a8554689e521a78316c27a99e9dd69ef33dc57
whywhathow <whywhathow.fun@gmail.com>
2023-05-08 09:04:45 +0800

    feat: messageBox for twitter-thread-reader.js

--- js/twitter-thread-reader.js
+++ js/twitter-thread-reader.js
@@ -3,7 +3,7 @@
 // @namespace    https://whywhathow.github.io/
 // @homepage     https://github.com/WhyWhatHow/powertoys4browser
 // @supportURL   https://github.com/WhyWhatHow/powertoys4browser/issues
-// @version      1.4
+// @version      1.5
 // @description  when we read twitter thread, it's sucks. so I try to generate twitter thread reader in one single page, so i can easily read and share. Anyway, it's easy to read twitter's thread.
 // @author       whywhathow
 // @match        https://twitter.com/
@@ -69,10 +69,12 @@ function running() {
         onload: function (response) {
             //  页面跳转
             var redirectUrl = "https://twitter-thread.com/t/" + threadId;
+            showMessage("success", "--- enjoy ^_^ ---")
             redirect(redirectUrl);
             console.log(response.responseText); // 打印响应内容
         },
         onerror: function (error) {
+            showMessage("error","Load page error, please try again!")
             console.error(error);
         }
     });
@@ -90,6 +92,72 @@ function isInTwitterInput() {
         return false;
     }
 }
+/**
+ * 创建 消息通知组件:
+ * @param type :类型
+ * @returns {HTMLDivElement}
+ */
+function createMessageElement(type) {
+    const messageElement = document.createElement('div');
+    messageElement.style.position = 'fixed';
+    messageElement.style.top = '20px';
+    messageElement.style.right = '20px';
+    messageElement.style.zIndex = '9999';
+    messageElement.style.width = '350px';
+    messageElement.style.maxWidth = '80%';
+    messageElement.style.padding = '15px 50px 15px 15px';
+    messageElement.style.color = '#fff';
+    messageElement.style.fontSize = '14px';
+    messageElement.style.lineHeight = '1.5';
+    messageElement.style.borderRadius = '4px';
+    messageElement.style.boxShadow = '0 2px 12px 0 rgba(0, 0, 0, 0.1)';
+    messageElement.style.transition = 'opacity .3s, transform .3s';
+    // messageElement.style.opacity = '0';
+    messageElement.style.transform = 'translate3d(0, -50%, 0)';
+    messageElement.style.pointerEvents = 'none';
+    // 根据消息类型设置不同的 backgroudColor
+    switch (type) {
+        case 'success':
+            messageElement.style.backgroundColor = '#67C23A'; // 成功消息背景色
+            break;
+        case 'info':
+            messageElement.style.backgroundColor = '#909399'; // 信息消息背景色
+            break;
+        case 'error':
+            messageElement.style.backgroundColor = '#F56C6C'; // 错误消息背景色
+            break;
+        default:
+            break;
+    }
+    messageElement.style.color = '#fff';
+    messageElement.style.fontWeight = 'bold';
+
+    return messageElement;
+}
+
+/**
+ * 显示消息
+ * @param type
+ * @param message
+ * @param timeout
+ */
+function showMessage(type, message, timeout=1000) {
+    const container = createMessageElement(type);
+    const content = document.createElement('p');
+    content.style.margin = '0';
+    content.innerText = message;
+    container.appendChild(content);
+
+    document.body.appendChild(container);
+    setTimeout(() => {
+        container.style.opacity = '0';
+        container.style.transform = 'translate3d(0, -50%, 0)';
+        container.style.pointerEvents = 'none';
+        container.remove();
+    }, timeout);
+}
+
+
 
 (function () {
     'use strict';
@@ -104,6 +172,7 @@ function isInTwitterInput() {
             // 执行您的脚本逻辑
             console.log('f------------------------------------f');
             // alert("just wait")
+            showMessage("info","Loading Page...",1000)
             running()
         }
 
