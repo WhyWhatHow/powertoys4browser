7aaa94098d1d8e814331c83c737d14e1bb4f700c
whywhathow <whywhathow.fun@gmail.com>
2023-07-04 17:29:44 +0800

    doc: - media_enhancer.ico - template.js for quickly add information about script.

---
+++ js/template.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         todo
+// @grant        none
+/// @run-at      todo  document-start
+// @icon         https://raw.githubusercontent.com/WhyWhatHow/powertoys4browser/master/icons/todo
+// @namespace    https://whywhathow.github.io/
+// @homepage     https://github.com/WhyWhatHow/powertoys4browser
+// @supportURL   https://github.com/WhyWhatHow/powertoys4browser/issues
+// @version      1.0
+
+// @author       whywhathow
+// @updateURL    https://raw.githubusercontent.com/WhyWhatHow/powertoys4browser/master/js/#todo
+// @license      MIT
+// ==/UserScript==
+
